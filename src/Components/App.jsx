fe6be320bf56e6623e47d99b137263c7d07d7c53
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-04-12 09:38:55 +1000

    Run prettier

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function App () {
-  return <h1>Project Planner Frontend</h1>
+export default function App() {
+  return <h1>Project Planner Frontend</h1>;
 }
