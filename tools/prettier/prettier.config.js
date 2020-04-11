fbe8c94b1b503d0c8b7f1d3b0e8dc0195eb63612
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-04-12 08:44:51 +1000

    Add prettier configsand npm scripts

---
+++ tools/prettier/prettier.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    endOfLine: 'lf',
+    jsxSingleQuote: true,
+    overrides: [
+      {
+        files: ['*.json', '*.yaml', '*.yml', '*.md', '*.markdown'],
+        options: {
+          singleQuote: false,
+          tabWidth: 4,
+          useTabs: false,
+        },
+      },
+    ],
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false,
+  };
+  
