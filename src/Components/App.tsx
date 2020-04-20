eff3a979a86c36bc7051e3d147504ded76ceef2a
Nizar El Berjawi <nizarberjawi12@gmail.com>
2020-04-20 22:28:02 +1000

    Create and use Mui Theme

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import Box from '@material-ui/core/Box';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
 export interface AppProps {
   name: string;
 }
 
-const App = ({ name }: AppProps): React.ReactElement => <h1>Hello {name}!</h1>;
+const App = ({ name }: AppProps): React.ReactElement => (
+  <ThemeProvider theme={theme}>
+    <Box component='h1' color={theme.palette.primary}>Hello {name}!</Box>
+  </ThemeProvider>
+);
 
 declare let module: object;
 
